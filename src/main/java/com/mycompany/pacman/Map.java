12d2403708f2ab90f47d6014024c7882c6b7bb4d
232578 <70134656+232578@users.noreply.github.com>
2021-03-05 21:23:54 +0100

    struktur fix

--- src/main/java/com/mycompany/pacman/Map.java
+++ src/main/java/com/mycompany/pacman/Map.java
@@ -3,41 +3,71 @@
  */
 package com.mycompany.pacman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author svein
  */
 public class Map extends MapStructure {
-    MapStructure ms;
     
     public Map(){
-       super();
+       setMapData(dataObject);
     }
     
-    public void generateMap() {
-        for(String[] sa: ms.getMapData()){
-            for(int i=0;i<sa.length; i++){
-                switch(sa[i]){
-                    case "K": // kant
-                        break;
-                    case "0": // kant inne i kartet
-                        break;
-                    case "A": // mat for pacman
-                        break;
-                    case "C": // "chase ability" for pacman
-                        break;
-                    case "Y": // ingen mat
-                        break;
-                    case "X": // tomt
-                        break;
-                    case "S": // spøkelse spawn
-                        break;
-                    case "U": // dør spøkelse spawn 
-                        break;
-                    case "B": // spøkelse bur
-                    default:
+    /**
+     * 
+     * Leser datastruktur fra fil som string
+     * Datastrukturen blir returnert som Object[],
+     * der objektene er String[]
+     * 
+     * @param path er filepath til standard kartfile
+     *
+     */
+    protected void readFile(String path){
+        ArrayList<String[]> al = new ArrayList<>();
+        try {
+            File file = new File(path);
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNext()) {
+                if(scanner.hasNextLine()){
+                    // lager liste av hvær linje i datastruktren fra fil
+                    al.add(scanner.nextLine().split(","));
                 }
+                
             }
+            
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        dataObject = al.toArray();
+    }
+    
+    /**
+     * Legger String[] fra dataObject super
+     * in i String[][] mapData super
+     * 
+     * @param oa er Object[] med String[] som objekter
+     */
+    protected final void setMapData(Object[] oa){
+        readFile(MAP_DATA_STRUCTURE_FILE);
+        mapData = new String
+            [oa.length]
+            [((String[])oa[0]).length];
+        for(int i=0; i<oa.length; i++){
+            mapData[i] = (String[])oa[i];
         }
     }
+    
+    public String[][] getMapData(){
+        return mapData;
+    }
+    
+    public void genrateNewMap() throws NoSuchMethodException {
+      throw new NoSuchMethodException("No methode");
+    }
+    
 }
--- src/main/java/com/mycompany/pacman/MapStructure.java
+++ src/main/java/com/mycompany/pacman/MapStructure.java
@@ -5,11 +5,7 @@
  */
 package com.mycompany.pacman;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 /**
  *
@@ -19,66 +15,23 @@ public class MapStructure {
     protected static final String DEFULT_FILE_PATH = "src/main/java/";
     protected static final String MAP_DATA_STRUCTURE_FILE = DEFULT_FILE_PATH +"default-map";
     protected Object[] dataObject;
-    private String[][] mapData;
+    protected String[][] mapData;
+    
+    public MapStructure(){
+        dataObject = null;
+        mapData = null;
+    }
     
     /**
      * default MapStructure
+     * @param dataObject
      */
-    public MapStructure(){
-        setMapData(dataObject);
+    
+    public MapStructure(Object[] dataObject){
+        this.dataObject = dataObject;
     }
     
-    /**
-     * Legger String[] fra dataObject
-     * in i String[][] mapData
-     * 
-     * @param oa er Object[] med String[] som objekter
-     */
-    protected final void setMapData(Object[] oa){
-        readFile(MAP_DATA_STRUCTURE_FILE);
-        mapData = new String
-            [oa.length]
-            [((String[])oa[0]).length];
-        for(int i=0; i<oa.length; i++){
-            mapData[i] = (String[])oa[i];
-        }
-    }
     
-    public String[][] getMapData(){
-        return mapData;
-    }
-    
-    public void genrateNewMap() throws NoSuchMethodException {
-      throw new NoSuchMethodException("No methode");
-    }
-    
-    /**
-     * 
-     * Leser datastruktur fra fil som string
-     * Datastrukturen blir returnert som Object[],
-     * der objektene er String[]
-     * 
-     * @param path er filepath til standard kartfile
-     *
-     */
-    public void readFile(String path){
-        ArrayList<String[]> al = new ArrayList<>();
-        try {
-            File file = new File(path);
-            Scanner scanner = new Scanner(file);
-            while(scanner.hasNext()) {
-                if(scanner.hasNextLine()){
-                    // lager liste av hvær linje i datastruktren fra fil
-                    al.add(scanner.nextLine().split(","));
-                }
-                
-            }
-            
-        }catch(FileNotFoundException e){
-            System.out.println("File not found");
-        }
-        dataObject = al.toArray();
-    }
     
     @Override
     public String toString(){
